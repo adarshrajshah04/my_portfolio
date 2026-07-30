61a6b0901a952012a63fe2b3b654f3957c7d7507
adarshrajshah04 <adarshrajshah04@gmail.com>
2026-07-30 14:03:02 +0530

    add footer section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Skills from './components/Skills'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
      <Experience/>
      <Projects/>
      <Contact/>
+     <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from '../assets/CSS/Footer.module.css'
+const Footer = () => {
+  return (
+    <div>
+      <h3 className={classes.text}>© 2026 Adarsh Raj Shah. All rights reserved.</h3>
+    </div>
+  )
+}
+
+export default Footer
