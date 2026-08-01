d1257c0e654bb758a498f48503a75df4045a04d6
adarshrajshah04 <adarshrajshah04@gmail.com>
2026-08-01 14:21:01 +0530

    fix navbar responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useRef } from 'react'
+
 
 
 const Nav = () => {
+  const listShow = useRef(null)
+  
+  function bar(){
+    listShow.current.classList.toggle('active')
+  
+  }
+
+  const headingRef  = useRef(null)
+
+  window.onscroll=function(){
+    let abc= document.documentElement
+    
+    let h=headingRef.current
+
+    if(abc.scrollTop>250){
+      h.classList.add('change')
+    }else{
+      h.classList.remove('change')
+    }
+    
+    
+  }
+
+ 
+
   return (
-    <div className='nav_main'>
+    <div ref={headingRef} className='nav_main'>
       <div className='nav_logo'>
         <h1>Portfolio</h1>
+        <div className='nav_bar' onClick={bar}><i class="fa-solid fa-bars"></i></div>
       </div>
 
-      <div className='nav_list'>
+      <div ref={listShow} className='nav_list'>
         <a href="#Home"><li>Home</li></a>
         <a href="#About"><li>About</li></a>
         <a href="#Skills"><li>Skills</li></a>
@@ -21,7 +48,7 @@ const Nav = () => {
        <a href="#contact"> <button className='nav_btn'>Lets's Talk  <i className="bi bi-arrow-up-right"></i></button></a>
       </div>
 
-      <div className='nav_bar'><i class="fa-solid fa-bars"></i></div>
+      
     </div>
   )
 }
