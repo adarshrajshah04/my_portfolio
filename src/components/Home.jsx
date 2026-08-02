98101e9763cccdba410e8f5c700f8b4c53334e69
adarshrajshah04 <adarshrajshah04@gmail.com>
2026-08-02 14:06:19 +0530

    add resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import adarsh from "../assets/images/adarsh.jpg";
 import classes from "../assets/CSS/Home.module.css";
-// import resume from '../assets/resume/Adarsh_resume.pdf'
+import resume from '../assets/resume/Adarsh_resume.pdf'
 
 const Home = () => {
   return (
@@ -39,8 +39,7 @@ const Home = () => {
         </button>
         </a>
 
-       {/* <a  href={resume} download='Adarsh_Resume.pdf'> */}
-       <a  >
+       <a  href={resume} download='Adarsh_Resume.pdf'>
          <button className={classes.btn3}>
           <i class="bi bi-download"></i> My Resume
         </button>
